b840c255d0362f70fcb745c307ecd4126026d711
Christian Lück <christian@lueck.tv>
2019-09-05 16:29:03 +0200

    Update to support PHPUnit 7, PHPUnit 6 and PHPUnit 5

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+namespace Clue\Tests\React\Tar;
 
-error_reporting(-1);
-
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function expectCallableOnce()
     {
